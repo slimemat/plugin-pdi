f5b43585e19ef3872c159bcc1aebb9be0bee91cf
slimemat <buenomatheus@gmail.com>
2021-08-31 14:28:32 -0300

    Adição quase responder quando aluno

--- pdistudent.php
+++ pdistudent.php
@@ -32,17 +32,22 @@ require_once('../../config.php');
 require_once($CFG->dirroot . '/local/pdi/classes/forms/auth_student.php');
 require_login();
 
+require_once('print/trialsfunctions.php');
+
 $PAGE->set_url(new moodle_url('/local/pdi/pdistudent.php'));
 $PAGE->set_context(\context_system::instance());
 $PAGE->set_title("PDI Student");
 $PAGE->set_heading('PDI Student');
 $PAGE->requires->jquery();
-$PAGE->requires->js(new moodle_url($CFG->dirrroot . '/local/pdi/scripts/pdiscript.js'));
 
 global $USER, $DB;
 
 
 //page setup
+
+$blocoHtml = mostrarBlocosTrial();
+
+///
 
 
 //page
@@ -62,29 +67,27 @@ echo "<br>";
 echo "<div id='my-centralizadora'>";
 echo "<div id='div-dashboard-list'>"; //div dashboard list
 
-echo "<div class='my-margin-box'>
-<span class=\"my-circle\" style=\"background-color: var(--mysuccess); color: var(--myblack);\">✔</span>
-<div class='my-sidetext'>
-<span class='my-circle-title'>IT Trial</span>
-<p>07/07/2021 - 10/07/2021</p>
-</div>
+
+
+/////////
+echo "<span id='bloco-div-1'>";
+
+  echo "<div class='my-margin-box'>
+  <span class=\"my-circle\" style=\"background-color: var(--mysuccess); color: var(--myblack);\">✔</span>
+  <div class='my-sidetext'>
+  <span class='my-circle-title'>IT Trial</span>
+  <p>07/07/2021 - 10/07/2021</p>
+  </div>
+  </div>";
+
+  echo $blocoHtml;
+
+echo "</span>";
+
+echo "<div class='my-scroll my-hidden mx-auto' id='scroll-div-2' style='max-width: 50%';>
 </div>";
 
-echo "<div class='my-margin-box'>
-<span class=\"my-circle\" style=\"background-color: var(--mysuccess); color: var(--myblack);\">✔</span>
-<div class='my-sidetext'>
-<span class='my-circle-title'>Title with max of 25 chara</span>
-<p>07/07/2021 - 10/07/2021</p>
-</div>
-</div>";
-
-echo "<div class='my-margin-box'>
-<span class=\"my-circle\" style=\"background-color: var(--myerror); color: var(--myblack);\">✖</span>
-<div class='my-sidetext'>
-<span class='my-circle-title'>Title with max of 25 or so</span>
-<p>07/07/2021 - 10/07/2021</p>
-</div>
-</div>";
+//////
 
 //btn SHOW ALL
 echo "<div id='div-save-buttons'>";
@@ -114,8 +117,41 @@ echo $OUTPUT->footer();
 
 $(document).ready(function() {
 
-$( ".my-margin-box" ).on( "click", function() {
-  window.location.href = "studenttrial.php";
+$( ".my-round-card" ).on( "click", function() {
+  //window.location.href = "studenttrial.php";
+  
+  var idtrial = $(this).attr("data-idtrial");
+    
+    //passar o valor pro form
+    $("#hidden-trialid").val(idtrial);
+    
+    //ajax
+    var dados = $("#frm-trial-id").serialize();
+
+    $.ajax({
+        method: 'POST',
+        url: 'print/selectquestionstrial.php',
+        data: dados,
+
+        beforeSend: function(){
+            $("#scroll-div-2").html("<h3>loading...</h3>");
+            $("#bloco-div-1").hide();
+        }
+    })
+    .done(function(msg){
+        $("#scroll-div-2").show();
+        $("#scroll-div-2").html(msg);
+
+        $("#btn_salvar").prop("disabled",false);
+        $("#btn_finalizar").prop("disabled",false);
+
+    })
+    .fail(function(){
+        $("#scroll-div-2").html("Falha ao carregar!");
+        $("#bloco-div-1").show();
+    });
+
+
 });
 
 $( "#id_show_btn" ).on( "click", function() {
---
+++ print/trialsfunctions.php
@@ -0,0 +1,157 @@
+<?php
+// This file is part of Moodle Course Rollover Plugin
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     local_pdi
+ * @author      Matheus
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @var stdClass $plugin
+ */
+
+
+function mostrarBlocosTrial(){
+    global $USER, $DB;
+
+    //então, usar o setor para fazer outra pesquisa (sectorid)
+    $sql = "SELECT mdb.timecreated, mdb.dbid, mdb.smemberid, sm.sectorid, sm.userid as userid_sector_member, sm.trialid,trev.cohortid, cm.userid as userid_cohort_member, t.title as trialtitle, td.isstarted as trialisstarted, td.startdate, td.enddate, us.firstname, us.lastname
+    FROM {local_pdi_sect_mem_db} mdb
+    LEFT JOIN {local_pdi_sector_member} sm
+    ON sm.id = mdb.smemberid
+    LEFT JOIN {local_pdi_trial_evaluator} trev
+    ON trev.trialid = sm.trialid
+    LEFT JOIN {cohort_members} cm
+    ON cm.cohortid = trev.cohortid
+    LEFT JOIN {local_pdi_trial} t
+    ON t.id = sm.trialid
+    LEFT JOIN {local_pdi_trial_detail} td
+    ON td.trialid = t.id
+    LEFT JOIN {user} us
+    ON us.id = sm.userid
+    WHERE cm.userid = \"$USER->id\"
+    ";
+
+    $res = $DB->get_records_sql($sql);
+    //var_dump($res);
+    
+    //setup var
+    $blocoHtml = '';
+    $count_i = 0;
+    $count_trial = 0;
+
+    $xtrialid = '';
+    $xtrialtitle = '';
+    $xdbid = '';
+    $xsectorid ='';
+    $xuserid_sector_member ='';
+    $xfullevaluatorname = '';
+
+    $lastTrialid = 'null';
+    $lastFullevaluatorname = 'null';
+
+    foreach($res as $r){
+
+       $xtrialid = "$r->trialid";
+       $xtrialtitle = "$r->trialtitle";
+       $xdbid = "$r->dbid";
+       $xsectorid ="$r->sectorid";
+       $xuserid_sector_member ="$r->userid_sector_member";
+       $xfullevaluatorname = "$r->firstname" . " ". "$r->lastname";
+
+       
+       //verificar se esse está marcado como respondido
+       //não importa o setor, então está agrupado
+       $respondido = 0;
+
+       $respSQL = "SELECT * FROM {local_pdi_answer_status} pas
+                   WHERE pas.userid = '$USER->id'
+                   and pas.idtrial = '$xtrialid'
+                   GROUP BY pas.userid";
+       $respRes = $DB->get_records_sql($respSQL);
+
+       if(count($respRes)>0){
+          foreach($respRes as $rs){ $respondido = $rs->isfinished; }
+       }
+       
+
+       if($respondido == 0){
+       
+          if($xtrialid != $lastTrialid){
+             $blocoHtml .= "</span></span>";
+
+             $lastFullevaluatorname = 'null';
+          }
+          
+          if($xtrialid != $lastTrialid){
+
+             $blocoHtml .= "  
+             
+             <span class='my-round-card' id='trial_$xtrialid' data-idtrial='$xtrialid'>
+             <span class=\"my-circle\" style=\"background-color: var(--myerror); color: var(--myblack);\">✖</span>
+             
+             ";
+   
+             $blocoHtml .= "<span class='my-sidetext'>
+             <h5 class='my-font-family' title='nome do processo'>$xtrialtitle</h5>";
+             $blocoHtml .= "<span class='my-label'>Avaliadores:</span><br>";
+    
+             $count_trial++;
+          }
+
+          if($xfullevaluatorname != $lastFullevaluatorname){
+             $blocoHtml .= "<span class='my-mention'>$xfullevaluatorname</span>";
+          }
+          else{
+             $blocoHtml .= "</div>";
+          }
+
+          $blocoHtml .= "
+          <span class='my-hidden' id='data-block-$xtrialid'>
+             <span class='processo-id-$xtrialid'>$xtrialid</span> 
+             <span class='responder-db-id-$xtrialid'>$xdbid</span> 
+             <span class='setor-id-$xtrialid'>$xsectorid</span>
+             <span class='avaliador-id-$xtrialid'>$xuserid_sector_member</span>
+          </span>";
+
+          //guardar os valores anteriores
+          $lastTrialid = $xtrialid;
+          $lastFullevaluatorname = $xfullevaluatorname;
+          
+          $count_i++;
+       }
+    }
+
+    if($respondido == 0){
+
+       $blocoHtml .= "</span></span>";
+    }
+    
+    $blocoReturn = "
+    <!--opened popup-->
+        
+          
+             $blocoHtml
+             
+      <form id='frm-trial-id' name='frm-trial-id' class='my-hidden' method='POST' action=''>
+         <input type=\"hidden\" name=\"hidden-trialid\" id=\"hidden-trialid\" value=\"\">
+         <input type=\"hidden\" name=\"hidden-currentuserid\" id=\"hidden-currentuserid\" value=\"$USER->id\">
+      </form>
+    ";
+
+    return $blocoReturn;
+
+
+}
+
